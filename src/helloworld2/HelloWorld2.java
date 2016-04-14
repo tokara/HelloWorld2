c62e2ae5334e106d81f7c28a63adf83119eabe40
User <User@194.47.44.94>
2016-04-14 11:30:23 +0200

    new commit

--- src/helloworld2/HelloWorld2.java
+++ src/helloworld2/HelloWorld2.java
@@ -15,7 +15,7 @@ public class HelloWorld2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    System.out.println("helloworld222");
+    System.out.println("helloworld");
     }
    
     
